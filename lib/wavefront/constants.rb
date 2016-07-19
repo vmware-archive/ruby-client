9892bd4b14f0fa4b3561dab874a012e6d3f0d31d
Robert Fisher <slackboy@gmail.com>
2016-07-19 22:25:41 +0100

    sensible defaults for proxy

--- lib/wavefront/constants.rb
+++ lib/wavefront/constants.rb
@@ -30,5 +30,7 @@ module Wavefront
     GRANULARITIES = %w( s m h d )
     EVENT_STATE_DIR = Pathname.new('/var/tmp/wavefront/events')
     EVENT_LEVELS = %w(info smoke warn severe)
+    DEFAULT_PROXY = 'wavefront'
+    DEFAULT_PROXY_PORT = 2878
   end
 end
